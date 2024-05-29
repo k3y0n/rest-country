3b78990f95628ab3cec6f864b16d6a5f27d755ef
k3y0n <y3.0n@yandex.by>
2024-05-29 15:57:16 +0300

    finish project

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Country from "../pages/Country";
+import NotFound from "../pages/NotFound";
 
 export const router = createBrowserRouter([
     {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
     {
         path: "/country/:name",
         element: <Country />,
+    },
+    {
+        path: "*",
+        element: <NotFound />,
     },
 ]);
